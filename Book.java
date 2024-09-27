768818f4e992718ddeb3c27c3e4399c246c02e74
samrudhi0511 <133109624+samrudhi0511@users.noreply.github.com>
2024-09-27 07:30:04 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,36 @@
+package com.library;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    // Getters and equals() method
+    public String getIsbn() { return isbn; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public int getPublicationYear() { return publicationYear; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return isbn.equals(book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return isbn.hashCode();
+    }
+}
